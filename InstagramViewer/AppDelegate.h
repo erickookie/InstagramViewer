ddcb0345747c67bdd849617f3a73a2acbcc82e08
erickookie <yue.eyqt@gmail.com>
2015-08-18 09:43:37 -0400

    Initial Commit

---
+++ InstagramViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstagramViewer
+//
+//  Created by MCS on 8/18/15.
+//  Copyright (c) 2015 MCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
